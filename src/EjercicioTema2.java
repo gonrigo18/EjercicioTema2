77a68e9a055e9b6323f2bbc0f47ca7ea83fd28e2
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2022-08-16 15:38:19 -0300

    Second commit

--- src/EjercicioTema2.java
+++ src/EjercicioTema2.java
@@ -1,12 +1,15 @@
+package EjercicioTema2;
+import java.util.Scanner;
 public class EjercicioTema2 {
-
     public static void main(String[] args) {
-    iva(100);
-    }
-    public static void iva (int monto){
-        int iva = (monto*21)/100;
-        int total = monto + iva;
-        System.out.println(total);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un monto: ");
+
+        int monto = scanner.nextInt();
+        double iva = monto * 0.21;
+        double sumaConIva = monto + iva;
+        System.out.println("Total con IVA: " + sumaConIva);
 
     }
 }
